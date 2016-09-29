a5edeb97671a526587b7aa03d1c55aa15d0de3e3
Sasikumar Mani <sasikumarmofs1@gmail.com>
2016-09-29 18:27:18 -0400

    Code Refactor

--- src/main/scala/com/objectfrontier/healthcare/main/CassandraConnector.scala
+++ src/main/scala/com/objectfrontier/healthcare/main/CassandraConnector.scala
@@ -15,118 +15,68 @@ import com.websudos.phantom.reactivestreams._
 object CassandraConnecter {
   def main(args: Array[String]) {
 
-    var startTotTime = System.currentTimeMillis()
-    var totalRecords = 0
+    var startTime = System.currentTimeMillis()
     
-    //AdmissionsCorePopulatedTable.txt File Reading
-    var file = Source.fromFile("/home/ksaha/data/100-Patients/AdmissionsCorePopulatedTable.txt")
+    insertRecords("/home/ksaha/data/100-Patients/AdmissionsCorePopulatedTable.txt","Admission")
+    
+    insertRecords("/home/ksaha/data/100-Patients/AdmissionsDiagnosesCorePopulatedTable.txt","AdmissionDiagnoses")
+    
+    insertRecords("/home/ksaha/data/100-Patients/LabsCorePopulatedTable.txt","Lab")
+    
+    insertRecords("/home/ksaha/data/100-Patients/PatientCorePopulatedTable.txt","Patient")
+
+   
+    println(" \n\t\tTotal Time Taken:" + (System.currentTimeMillis() - startTime) / 1000 + " seconds")
+    
+  }
+  
+ def insertRecords(fileName: String,model: String): Boolean = {
+    var file = Source.fromFile(fileName)
+    
     var startTime = System.currentTimeMillis()
     var i = 0
 
     for (line <- file.getLines.drop(1)) {
       
-      var admissionsList = line.split('\t')
+      var columnsList = line.split('\t')
       
-      if(admissionsList.length == 4){
-         AdmissionService.saveOrUpdate(Admission(UUID.fromString(admissionsList(0)), admissionsList(1).toInt, admissionsList(2), admissionsList(3)))
+      if(model == "Admission"){
+        
+        if(columnsList.length == 4){
+           AdmissionService.saveOrUpdate(Admission(UUID.fromString(columnsList(0)), columnsList(1).toInt, columnsList(2), columnsList(3)))
+        }
+        
+      } else  if(model == "AdmissionDiagnoses"){
+        
+        if(columnsList.length == 4){
+           AdmissionDiagnoseService.saveOrUpdate(AdmissionDiagnose(UUID.fromString(columnsList(0)), columnsList(1).toInt, columnsList(2), columnsList(3)))
+        }
+        
+      } else  if(model == "Lab"){
+      
+        if(columnsList.length == 6){
+           LabService.saveOrUpdate(Lab(UUID.fromString(columnsList(0)), columnsList(1).toInt, columnsList(2), columnsList(3).toFloat,columnsList(4),columnsList(5)))
+        }
+        
+      } else  if(model == "Patient"){
+      
+        if(columnsList.length == 7){
+           PatientService.saveOrUpdate(Patient(UUID.fromString(columnsList(0)), columnsList(1), columnsList(2), columnsList(3),columnsList(4),columnsList(5),columnsList(6).toFloat))
+        }
       }
+      
       i = i + 1
     }
     
     var endTime = System.currentTimeMillis()
     var seconds = (endTime - startTime) / 1000;
-    totalRecords =i;
 
     println(" \n\t\t***************************")
-    println(" \n\t\tTotal Admissions No of Records:" + i)
+    println(" \n\t\tTotal "+ model+" No of Records:" + i)
     println(" \n\t\tTotal Time Taken:" + seconds + " seconds")
     println(" \n\t\t***************************")
 
     file.close 
-    
-    //AdmissionsDiagnosesCorePopulatedTable.txt File Reading
-    file = Source.fromFile("/home/ksaha/data/100-Patients/AdmissionsDiagnosesCorePopulatedTable.txt")
-    startTime = System.currentTimeMillis()
-    i = 0
-
-    for (line <- file.getLines.drop(1)) {
-      
-      var admissionDiagnoseList = line.split('\t')
-      
-      if(admissionDiagnoseList.length == 4){
-         AdmissionDiagnoseService.saveOrUpdate(AdmissionDiagnose(UUID.fromString(admissionDiagnoseList(0)), admissionDiagnoseList(1).toInt, admissionDiagnoseList(2), admissionDiagnoseList(3)))
-      }
-      i = i + 1
-    }
-    
-    endTime = System.currentTimeMillis()
-    seconds = (endTime - startTime) / 1000;
-    totalRecords = totalRecords +i;
-
-    println(" \n\t\t***************************")
-    println(" \n\t\tTotal Admission Diagnose No of Records:" + i)
-    println(" \n\t\tTotal Time Taken:" + seconds + " seconds")
-    println(" \n\t\t***************************")
-
-    file.close
-    
-    //LabsCorePopulatedTable.txt File Reading
-    file = Source.fromFile("/home/ksaha/data/100-Patients/LabsCorePopulatedTable.txt")
-    startTime = System.currentTimeMillis()
-    i = 0
-
-    for (line <- file.getLines.drop(1)) {
-      
-      var labList = line.split('\t')
-      
-      if(labList.length == 6){
-         LabService.saveOrUpdate(Lab(UUID.fromString(labList(0)), labList(1).toInt, labList(2), labList(3).toFloat,labList(4),labList(5)))
-      }
-      i = i + 1
-    }
-    endTime = System.currentTimeMillis()
-    seconds = (endTime - startTime) / 1000;
-    totalRecords = totalRecords +i;
-
-    println(" \n\t\t***************************")
-    println(" \n\t\tTotal Lab No of Records:" + i)
-    println(" \n\t\tTotal Time Taken:" + seconds + " seconds")
-    println(" \n\t\t***************************")
-
-    file.close
-    
-    //PatientCorePopulatedTable.txt File Reading
-    file = Source.fromFile("/home/ksaha/data/100-Patients/PatientCorePopulatedTable.txt")
-    startTime = System.currentTimeMillis()
-    i = 0
-
-    for (line <- file.getLines.drop(1)) {
-      
-      var patientsList = line.split('\t')
-      
-      if(patientsList.length == 7){
-         PatientService.saveOrUpdate(Patient(UUID.fromString(patientsList(0)), patientsList(1), patientsList(2), patientsList(3),patientsList(4),patientsList(5),patientsList(6).toFloat))
-      }
-      i = i + 1
-    }
-    endTime = System.currentTimeMillis()
-    seconds = (endTime - startTime) / 1000;
-    totalRecords = totalRecords +i;
-
-    println(" \n\t\t***************************")
-    println(" \n\t\tTotal Paitents No of Records:" + i)
-    println(" \n\t\tTotal Time Taken:" + seconds + " seconds")
-    println(" \n\t\t***************************")
-
-    file.close
-    
-    var endTotTime = System.currentTimeMillis()
-    var totSeconds = (endTotTime - startTotTime) / 1000;
-    
-    println(" \n\t\t***************************")
-    println(" \n\t\tTotalNo of Records:" + totalRecords)
-    println(" \n\t\tTotal Time Taken:" + totSeconds + " seconds")
-    println(" \n\t\t***************************")
-    
-  }
+    return true
+ }
 }
